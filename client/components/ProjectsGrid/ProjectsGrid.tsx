601bafe6786f147540d1ea1585c98b610f264e23
SamikQ <samokhvalov.dmitriy@gmail.com>
2025-07-04 23:07:52 +0300

    grid upd

--- client/components/ProjectsGrid/ProjectsGrid.tsx
+++ client/components/ProjectsGrid/ProjectsGrid.tsx
@@ -12,30 +12,40 @@ import FadeInSection from "@/shared/UI/FadeInSection/FadeInSections";
 
 const projects = [
   {
+    id: 1,
+    area: "cards-one",
     title: "Project 1",
     image: project1,
     link: "/projects/1",
     alt: "Project 1 preview",
   },
   {
+    id: 2,
+    area: "cards-two",
     title: "Project 2",
     image: project2,
     link: "/projects/2",
     alt: "Project 2 preview",
   },
   {
+    id: 3,
+    area: "cards-three",
     title: "Project 3",
     image: project3,
     link: "/projects/3",
     alt: "Project 3 preview",
   },
   {
+    id: 4,
+    area: "cards-four",
     title: "Project 4",
     image: project4,
     link: "/projects/4",
     alt: "Project 4 preview",
   },
   {
+    id: 5,
+    area: "cards-five",
     title: "Project 5",
     image: project5,
     link: "/projects/5",
@@ -44,58 +54,29 @@ const projects = [
 ];
 
 const ProjectsGrid: React.FC = () => (
-  <section className={styles.container}>
+  <section className="container">
     <SectionTitle alignment="left">Наші проєкти</SectionTitle>
 
     <FadeInSection>
       <div className={styles.grid}>
-        {projects.slice(0, 2).map((project) => (
-          <a
-            key={project.title}
-            href={project.link}
-            className={styles.card}
-            aria-label={project.title}
-            tabIndex={0}
-          >
-            <Image
-              src={project.image}
-              alt={project.alt}
-              className={styles.image}
-            />
-          </a>
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className={`${styles.card} ${project.area}`}
+            style={{ gridArea: project.area }}>
+            <Image src={project.image} alt={project.alt} />
+            <div className={styles.overlay}>
+              <h3>{project.title}</h3>
+            </div>
+          </div>
         ))}
       </div>
     </FadeInSection>
-
-    <FadeInSection>
-      <div className={styles.grid}>
-        {projects.slice(2).map((project, index) => (
-          <a
-            key={project.title}
-            href={project.link}
-            className={styles.card}
-            aria-label={project.title}
-            tabIndex={0}
-            style={{
-              gridColumn: index === 0 || index === 1 ? "span 2" : "span 1",
-            }}
-          >
-            <Image
-              src={project.image}
-              alt={project.alt}
-              className={styles.image}
-            />
-          </a>
-        ))}
-      </div>
-    </FadeInSection>
-
     <FadeInSection>
       <a
         href="/projects"
         className={styles.moreBtn}
-        aria-label="Більше проєктів"
-      >
+        aria-label="Більше проєктів">
         <span className={styles.moreText}>Більше проєктів</span>
         <img
           src="/assets/icons/arrow-2-right-long.svg"
---
+++ client/shared/UI/Button/index.tsx
@@ -0,0 +1,38 @@
+import classNames from "classnames";
+import style from "./index.modules";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "small" | "large";
+  variant?: "primary" | "secondary" | "tertiary";
+  isLoading?: boolean;
+  fullWidth?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  size = "large",
+  variant = "primary",
+  isLoading = false,
+  fullWidth,
+  className,
+  ...props
+}) => {
+  const buttonClass = classNames(
+    style.button,
+    style[size],
+    style[variant],
+    {
+      [style.loading]: isLoading,
+    },
+    {
+      [style.full_width]: fullWidth,
+    },
+    className
+  );
+  return (
+    <button className={buttonClass} {...props}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
